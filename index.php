9a2a10450e45a60bcfaa19a250c2a46668ec5d75
Pavel Ivanchenko <shaman1186@ya.ru>
2012-01-15 23:25:27 +0200

    v3 обновлена структура сайта

--- index.php
+++ index.php
@@ -1,43 +1,32 @@
 <?php
-	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
-	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-	header("Pragma: no-cache"); // HTTP/1.0
 	//функция чтения данных из файла
 	function read_file($file)
 	{
-	fopen($file);
-	$text = file ($file);
-	for ($i = count($text)-1; $i >= 0; --$i)
+		fopen($file);
+		$text = file($file);
+		for ($i = count($text)-1; $i >= 0; --$i)
 		{
 			list ($name, $msg) = split('[|]', $text[$i]);
 			$msg = rtrim($msg);
 			$ii = $i+1;
-			$ct .= "Сообщение №$ii: \t\"$name : $msg\"\n";
+			$ct .= "\t\t\t\t\t\t<dt>Сообщение №$ii:\t$name :</dt>\n";
+			$ct .= "\t\t\t\t\t\t<dd>$msg</dd>\n";
 		}
-	return $ct;		
+		return $ct;		
 	}
-	//функция обработки ошибок
-	function error($id)
+	//функция "очистка истории"
+	function clear_history($file)
 	{
-	if ($id == 1)
-		{
-		$error_msg = "Введите Ваше имя";
-		}
-	if ($id == 2)
-		{
-		$error_msg = "Введите текст сообщения";
-		}
-	if ($id == 3)
-		{
-		$error_msg = "Введите Ваше имя и текст сообщения";
-		}
-	if ($id == 4)
-		{
-		$error_msg = "Вы ввели неправильный пароль";
-		}
-	return $error_msg;		
-	}
+		fopen($file, "w");
+		fclose($file);
+	};
+	//функция записи в файл
+	function write_to_file($file, $name, $msg)
+	{
+		$add = "$name|";
+		$add .= "$msg\n";
+		file_put_contents($file, $add, FILE_APPEND | LOCK_EX);
+	}	
 	//блок объявления переменных
 	$months_rus = array(1 => 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
 	$data_file = 'r.tmp';
@@ -46,39 +35,34 @@
 	$dat .= "$months_rus[$m]";
 	$dat .= Date(" Y");
 	$tm = Date("H:i:s");
-	$error_id = $_GET['error'];
+	$file = 'r.tmp';
+	//удаляем лишние пробелы из начала и конца сообщения
+	$name = trim($_POST['name']);
+	//удаляем лишние пробелы из начала и конца сообщения 
+	$msg = trim($_POST['msg']);
+	$clear_button = $_POST['clear'];
+	$add_button = $_POST['addmsg'];
+	$psswrd = $_POST['psswrd'];
+	//очистка истории
+	if (@$clear_button && $psswrd == php)
+	{
+		clear_history($file);
+		header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?history_cleaned");
+	};
+	//блок логики
+	//запись в файл
+	if (@$add_button)
+	{
+		if ($name != "" && $msg != "")
+		{
+			write_to_file($file, $name, $msg);
+			header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?message_added");
+		}	
+	};
+	//чтение файла
 	$ct = read_file($data_file);
-	$error = error($error_id);
+	ob_start();
+	require_once "layout.php";
+	ob_end_flush();
+	exit();
 ?>
-<!DOCTYPE html>
-<html lang = "ru">
-	<head>
-		<title>Гостевая книга</title>
-		<link rel = "stylesheet" type = "text/css" href = "style/style.css">
-		<meta http-equiv = "Content-Type" content = "text/html; charset=utf-8">
-	</head>
-	<body>
-	<a href= "index.php"><h1>Добро пожаловать в гостевую книгу</h1></a>
-	<h2>Сегодня <? echo $dat ?> года. Время на Ваших часах <? echo $tm ?>.</h2>
-	<hr />
-	<h4><? echo "$error" ?></h4>
-	<p>
-	<textarea id = "txt" name = "text" rows = "10" cols = "100"><? echo "$ct" ?></textarea>
-	</p>
-	<hr />
-	<form action = "layout.php" method = "POST" name = "DataForm"> 
-		<p id = "nt">Введите Ваше <b>Имя:</b><br>
-		<input id = "np" maxlength = "20" size = "20" name = "name" value = "Имя пользователя"><br>
-		Введите Ваше <b>сообщение:</b><br>
-		<textarea id = "msgp" name = "msg" wrap = "virtual" cols = "100" rows = "2">Текст сообщения...</textarea><br>
-		<input id = "sbtn" value = "Отправить сообщение" type = "submit" name = "addmsg"></p>
-	</form>
-	<hr/>
-	<form action = "layout.php" method = "POST" name = "ClearForm"> 
-		<p><input id = "chbtn" value = "Очистить историю" type = "submit" name = "clear">
-		Пароль: 
-		<input type = "password" maxlength = "128" size = "20" name = "psswrd" value = "php"></p>
-	</form>
-	<h5>Maded by Shaman</h5>
-	</body>
-</html>
--- layout.php
+++ layout.php
@@ -1,47 +1,62 @@
-<?php
-$file = 'r.tmp';
-//удаляем лишние пробелы из начала и конца сообщения
-$name = ltrim(rtrim($_POST['name']));
-//удаляем лишние пробелы из начала и конца сообщения 
-$msg = ltrim(rtrim($_POST['msg']));
-$clear_button = $_POST['clear'];
-$add_button = $_POST['addmsg'];
-$psswrd = $_POST['psswrd'];
-$dat = Date("d-F-20y");
-$tm = Date("h:i:s");
-//очистка истории
-if (@$clear_button && $psswrd == php)
-	{
-	fopen($file, "w");
-	fclose($file);
-	header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?history_cleaned");
-	};
-if ($psswrd != php)
-	{
-	header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?error=4");
-	}
-//запись в файл
-if (@$add_button)
-	{
-	if ($name == "")
-		{
-		header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?error=1");
-		}
-	if ($msg == "")
-		{
-		header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?error=2");
-		}
-	if ($name == "" && $msg == "")
-		{
-		header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?error=3");
-		};
-	if ($name != "" && $msg != "")
-		{
-		$add  = "$name|";
-		$add .= "$msg\n";
-		file_put_contents($file, $add, FILE_APPEND | LOCK_EX);
-		header("Location: {$_SERVER['HTTP_HOSTS']}/index.php?message_added");
-		}	
-	};
-exit();
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Гостевая книга</title>
+		<link rel="stylesheet" href="style/style2.css"/>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+	</head>
+	<body>
+		<div id="header">
+			<h1>Добро пожаловать в гостевую книгу</h1>
+			<h2>Сегодня <? echo $dat ?> года. Время на Ваших часах <? echo $tm ?>.</h2>
+		</div>
+		<div id="content">
+			<div id="main">
+				<h2>Гостевая книга</h2>
+				<dl>
+					<? echo $ct ?>
+				</dl>
+			</div>
+			<div id="sections">
+				<h2>Ссылочки</h2>
+				<ul>
+					<li><a href="index.php">Решения</a></li>
+					<li><a href="index.php">Партнеры</a></li>
+					<li><a href="index.php">Достижения</a></li>
+				</ul>
+				<form id="search" action="#" method="get">
+					<p>Поиск по сайту</p>
+					<p><input type="text"/><button type="submit">Искать</button></p>
+				</form>
+			</div>
+			<div id="news">
+				<h2>Новости</h2>
+				<h3>01.01.06</h3>
+				<p>Мегакорпорация поздравляет своих клиентов с Новым Годом! Пусть в новом году ваши деньги перетекают в наши карманы еще быстрее. И больше!</p>
+		 	</div>
+		</div>
+		<div id="meta">
+			<form action="index.php" method="post" name="data_form"> 
+				<dl>
+					<dt><label for="name">Введите Ваше <strong>Имя:</strong> *</label></dt>
+					<dd><input type="text" maxlength="20" size="20" name="name" value="Имя"/></dd>
+				</dl>
+				<dl>
+					<dt><label for="msg">Введите Ваше <em>сообщение:</em> *</label></dt>
+					<dd><textarea name="msg" rows="2">Текст сообщения...</textarea></dd>
+				</dl>
+				<input id="button_send" value="Отправить сообщение" type="submit" name="addmsg"/>
+			</form>
+			<form action="index.php" method="post" name="clear_form"> 
+				<dl>
+					<dt>Пароль:</dt>
+					<dd><input type="password" maxlength="20" size="20" name="psswrd" value="php"></dd>
+				</dl>
+				<input id="button_clear"value="Очистить историю" type="submit" name="clear">
+			</form>
+		</div>
+		<div id="footer">
+			<p>Maded by Shaman &copy; 2012</p>		
+		</div>
+	</body>
+</html>
